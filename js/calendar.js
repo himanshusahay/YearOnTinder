182ce461b4d1fb27424fda6a91f6064bc78bb403
Himanshu Sahay <himanshusahay@github.com>
2017-04-27 20:35:43 -0700

    added data to client side

--- js/calendar.js
+++ js/calendar.js
@@ -1,46 +1,8 @@
-var BASE_URL = 'https://api.gotinder.com',
-  USER_ID = '10210713162919668',
-  ACCESS_TOKEN = 'EAAGm0PX4ZCpsBAMdya9AUCQcV5xgeZCz7vPskeqouiFvGo0imIZCqZCj4ytaFndXEHB4DexJZCN2YYZBKNvGpsjtCDcXIbnLCYuw4ZAwxLMKJFsifgJqJqUXZAkpeQ5JbPQ1hkMKh7chMpEXlLwyERRZBV1h66DL6GIZCgEDEyDgbqpJEfIZAR0XYOWfC7yNnlIw5dKlgFxd8cNLafWkaI6TfcnZCchjFgXZAPDOZBVLrY2sKiOSSPHi8ZAnFGoacrwruS3O6oZD';
-
-var tinderAPI = axios.create({
-  baseURL: BASE_URL,
-  headers: {
-    // 'X-Auth-Token': ACCESS_TOKEN,
-    'Content-type': 'application/json',
-    'User-agent': 'Tinder/4.7.1 (iPhone; iOS 9.2; Scale/2.00)',
-  }
-});
-
-// Tinder Wrapper Functions
-function tinderAuth() {
-  var data = {
-    'facebook_token': ACCESS_TOKEN,
-    'facebook_id': USER_ID
-  };
-  var endpoint = BASE_URL + '/auth';
-  return tinderAPI.post(endpoint, data);
-}
-
-function getUserAccount() {
-  return tinderAPI.get('/user/12345');
-}
-
-function getUserPermissions() {
-  return tinderAPI.get('/user/12345/permissions');
-}
-
-// Fetch data
-tinderAuth().then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
+var data;
+axios.get('/data.json')
+  .then(function (response) {
+    console.log(response.data);
   });
-
-// axios.all([getUserAccount(), getUserPermissions()])
-//   .then(axios.spread(function (acct, perms) {
-//     // Both requests are now complete
-//   }));
 
 // Build calendar
 var now = moment().endOf('day').toDate();
