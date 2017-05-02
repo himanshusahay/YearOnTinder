44fc653610ffb0ea721c8629a5f175ab35e32cb3
Tom Meagher <thom.meagher@gmail.com>
2017-05-02 18:42:59 -0400

    Data structure changes

--- js/scatter.js
+++ js/scatter.js
@@ -18,7 +18,7 @@ d3.json("data.json", function(data) {
 	var dateMap = {};
 	var personByDate = {};
 	
-	data["updates"]["matches"].forEach(function(d){
+	data.updates.matches.forEach(function(d){
 	  	// Save only 1 yes/no record for each day a message was sent
 	  	// Each record is a message:
 	  	// Save id, name, main image, sent date, message count
@@ -29,9 +29,13 @@ d3.json("data.json", function(data) {
 			  	
 			  	console.log(d.messages.length);
 			  	d.messages.forEach(function(message){
-					var unformattedMessageDate = moment(message.sent_date).format('l'); // 23/1/2017
-					var	parseDate = d3.time.format("%d/%m/%Y").parse;
-					var messageDate = parseDate(unformattedMessageDate);
+		          	var messageDate = moment(message.sent_date).format('ddd MMM DD YYYY');
+		          	messageDate += " 00:00:00 GMT-0400 (EDT)"; // To make date compatible with building calendar below
+
+
+					// var unformattedMessageDate = moment(message.sent_date).format('l'); // 23/1/2017
+					// var	parseDate = d3.time.format("%d/%m/%Y").parse;
+					// var messageDate = parseDate(unformattedMessageDate);
 					// console.log(messageDate);
 					var timeParse = d3.time.format("%H:%M:%SZ").parse;
 					// console.log(moment(message.timestamp).format('LTS'));
@@ -44,90 +48,83 @@ d3.json("data.json", function(data) {
 		  				personByDate[messageDate] = [];
 				  	}
 				  	if (!(d._id in personByDate[messageDate])){  
-			  			personByDate[messageDate].push((d._id, timeOfDay));
+			  			personByDate[messageDate].push({
+			  				id: d._id,
+			  				time: timeOfDay
+			  			});
 				  	}
 			  	});
 			  	
 			  	// Save details about this user (will only do this once)
-			  	if (!(d._id in dateMap)){
-			  		console.log("inside datemap");
-					dateMap["_id"] = {};
-					if (d.person !== undefined){
-					  	dateMap["_id"]["name"] = d.person.name;
-					  	if (d.person.photos.length > 0){
-						  	dateMap["_id"]["image-url"] = d.person.photos[0].url;
-					  	}
-					  	// If no image, save a static image
-					  	else{
-					  		dateMap["_id"]["image-url"] = "http://a3.mzstatic.com/us/r30/Purple19/v4/86/bf/53/86bf5324-e2fb-b196-aa1a-bcde5f0e84fe/screen696x696.jpeg";
-					  	}
-					  	dateMap["_id"]["message_count"] = d.messages.length;
-					}
-				} 
+			  	if (!(d._id in dateMap) && d.person !== undefined) {
+			  		var image_url = d.person.photos.length > 0 ? d.person.photos[0].url : "http://a3.mzstatic.com/us/r30/Purple19/v4/86/bf/53/86bf5324-e2fb-b196-aa1a-bcde5f0e84fe/screen696x696.jpeg";
+			  		dateMap[d._id] = {
+			  			id: d._id,
+			  			name: d.person.name,
+			  			image_url: image_url,
+			  			message_count: d.messages.length,
+			  			success_category: 1
+			  		}
+			  	}
 			}
 		}
 	});
 
 	console.log(dateMap);
 	// Get category of interaction success
-	dateMap.forEach(function(id){
-		if (id["message_count"] > 1){
+	Object.keys(dateMap).forEach(function (key){
+		match = dateMap[key];
+		console.log(data.updates.messages);
+		if (match.message_count > 1){
 			result = true;
-			id["success-category"] = -1;
 
-			// data["updates"]["messages"][id].forEach(function(message){
-			// 	result = libphonenumber.isPossibleNumber(libphonenumber.findNumbers(message["message"]));
-			// 	if (result === true){
-			// 		dateMap[id]["success-category"] = 5;
-			// 		break;  // no ability to break in forEach loop, see for of loop below
-			// 	}
-			// });
+			var messages = data.updates.matches.filter(function (m) {
+				return m.id === key;
+			})[0].messages;
 
-			for (let message of data["updates"]["messages"][id]) {
+			for (let message of messages) {
 				console.log(message);
-				result = libphonenumber.isPossibleNumber(libphonenumber.findNumbers(message["message"]));
-				if (result === true){
-					id["success-category"] = 5;
+				// result = libphonenumber.isPossibleNumber(libphonenumber.findNumbers(message.message));
+				result = false;
+				if (result){
+					match.success_category = 5;
 					break;
 				}
 			}
 
-			if (id["success-category"] === -1){
-				var message_count = id["message_count"];
+			if (match.success_category != 5){
+				var message_count = match.message_count;
 				if (message_count >= 10){
-					id["success-category"] = 4;
+					match.success_category = 4;
 				}
 				else if (message_count > 3){
-					id["success-category"] = 3;
+					match.success_category = 3;
 				}
 				else if (message_count > 2){
-					id["success-category"] = 2;
+					match.success_category = 2;
 				}
 			}
-		}
-		else{
-			id["success-category"] = 1
 		}
 	});
 
 	var y = d3.time.scale()
     .range([height, 0]).nice();
 
-	y.domain(personByDate.map(function(d){ return d[1] }));
+	y.domain(Object.keys(personByDate).map(function(d){ return d[1] }));
 
 	var xScale = d3.time.scale()
 		.range([0, width]).nice()
-		.domain(personByDate.map(function(d){ return d[0] }));
+		.domain(Object.keys(personByDate).map(function(d){ return d[0] }));
 
 	var xAxis = d3.svg.axis()
 		.scale(xScale)
 		.orient("bottom")
-		.ticksize(-height);
+		.tickSize(-height);
 
 	var yAxis = d3.svg.axis()
 		.scale(y)
 		.orient("left")
-		.ticksize(-width);
+		.tickSize(-width);
 
 	var color = d3.scale.category10();
 
@@ -135,11 +132,12 @@ d3.json("data.json", function(data) {
       .attr("class", "d3-tip")
       .offset([-10, 0])
       .html(function(d) {
-        return d["name"] + ",<br>";
+        return d.name + ",<br>";
       });
 
+
     var zoomBeh = d3.behavior.zoom()
-      .x(x)
+      .x(xScale)
       .y(y)
       .scaleExtent([0, 500])
       .on("zoom", zoom);
@@ -204,9 +202,9 @@ d3.json("data.json", function(data) {
 	  .data(personByDate)
 	  .enter().append("circle")
 	  .classed("dot", true)
-	  .attr("r", function (d) { return 6 * Math.sqrt(dateMap[d[0]]["message_count"] / Math.PI); })
+	  .attr("r", function (d) { return 6 * Math.sqrt(dateMap[d[0]].message_count / Math.PI); })
 	  .attr("transform", transform)
-	  .style("fill", function(d) { return color(dateMap[d[0]]["success-category"]); })
+	  .style("fill", function(d) { return color(dateMap[d[0]].success_category); })
 	  .on("mouseover", tip.show)
 	  .on("mouseout", tip.hide);
 
